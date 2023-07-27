5228399b177fb481a48adc331f646e085ad070c7
Lotho <58112436+MLotho@users.noreply.github.com>
2023-07-27 01:13:16 +0100

    init

---
+++ index.js
@@ -0,0 +1,69 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const Redis = require('ioredis');
+
+let window;
+let client;
+
+app.on('ready', () => {
+  window = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 800,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    resizable: false
+  });
+
+  const isDev = process.env.NODE_ENV == 'development';
+
+  console.log("isDev: ", isDev);
+  console.log(process.env.NODE_ENV)
+
+  if (isDev) {
+    window.loadURL('http://localhost:3000');
+    window.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    window.loadURL(`${app.getAppPath()}\\build\\index.html`);
+  }
+
+  ipcMain.on('redis-connect', (event, formData) => {
+    if (client?.connected) client.quit();
+
+    const { host, port, username, password } = formData;
+
+    // Create a Redis client in the main process
+    client = new Redis({
+      host,
+      port,
+      username,
+      password
+    });
+
+    client.on('error', (err) => {
+      console.log("error: ", err)
+    });
+
+    client.on('ready', () => {
+      window.webContents.send('redis-connection-status', true);
+    });
+
+    client.on('message', (channel, message) => {
+      window.webContents.send('redis-packet', message);
+    });
+  });
+
+  ipcMain.on('redis-subscribe', (event, channel) => {
+    client.subscribe(channel);
+  });
+
+  ipcMain.on('redis-unsubscribe', (event, channel) => {
+    client.unsubscribe(channel);
+  });
+
+  ipcMain.on('redis-disconnect', () => {
+    client.quit();
+    window.webContents.send('redis-connection-status', false);
+  })
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+
+import Highlight from 'react-highlight'
+import 'highlight.js/styles/obsidian.css'; // Import the CSS for the default highlight.js style
+import 'highlight.js/lib/languages/json'; // Import the JSON syntax highlighting for highlight.js
+import 'highlight.js/lib/languages/javascript'; // Import the JSON syntax highlighting for highlight.js
+
+const { ipcRenderer } = window.require('electron');
 
 function App() {
+  const [isConnected, setConnected] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [formData, setFormData] = useState({
+    host: '',
+    port: '',
+    username: '',
+    password: ''
+  });
+
+  const channelRef = React.useRef('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleConnect = () => ipcRenderer.send('redis-connect', formData);
+  const handleDisconnect = () => ipcRenderer.send('redis-disconnect');
+  const handleChannelChange = (e) => {
+    ipcRenderer.send('redis-unsubscribe', channelRef.current); // Unsubscribe from previous channel
+
+    const { value } = e?.target;
+    if (!value) return; // prevent subscribing to an empty string
+
+    channelRef.current = value;
+    ipcRenderer.send('redis-subscribe', value); // Use the value directly
+  };
+
+  React.useEffect(() => {
+    ipcRenderer.on('redis-packet', (event, message) => { // used to forward packets from current channel
+      const json = JSON.parse(message);
+      setMessages((prevMessages) => [...prevMessages, json]);
+    });
+
+    ipcRenderer.on('redis-connection-status', (event, isConnected) => { // used to determine if the client is connected or not
+      setMessages([]) // clear messages when the client connects or disconnects
+
+      if (isConnected) {
+        setFormData({ ...formData, host: '', port: '', username: '', password: '' });
+      }
+
+      setConnected(isConnected);
+    });
+
+    return () => {
+      ipcRenderer.removeAllListeners( // clean up the event listeners when the component unmounts
+        'redis-packet',
+        'redis-connection-status'
+      );
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <nav className={styles.navBar}>
+        <div className={styles.title}>Lettuce v0.1.0</div>
+        <div className={styles.buttons}>
+          <button className={styles.button + " " + styles.connectButton} onClick={() => isConnected ? handleDisconnect() : handleConnect()}>
+            {isConnected ? 'Disconnect' : 'Connect'}
+          </button>
+          {isConnected ? <button onClick={() => setMessages([])} className={styles.button}>Clear</button> : null}
+          {isConnected ? <input type="text" name="channel" placeholder="channel" onChange={handleChannelChange} className={styles.button} /> : null}
+        </div>
+      </nav>
+
+      <div className={styles.container}>
+        <form className={styles.credentials}>
+          <input
+            type="text"
+            name="host"
+            placeholder="host"
+            value={formData.host}
+            onChange={handleChange}
+            disabled={isConnected}
+          />
+          <input
+            type="text"
+            name="port"
+            placeholder="port"
+            value={formData.port}
+            onChange={handleChange}
+            disabled={isConnected}
+          />
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={formData.username}
+            onChange={handleChange}
+            disabled={isConnected}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={formData.password}
+            onChange={handleChange}
+            disabled={isConnected}
+          />
+        </form>
+
+        <div className={styles.codeContainer}>
+          <div className={styles.code}>
+            <Highlight className={styles.codeContent + " " + "json"}>
+              {messages.length > 0 ? JSON.stringify(messages, null, 2) : ( isConnected ? 'Waiting for packets...' : 'Connect to start receiving packets') }
+            </Highlight>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
